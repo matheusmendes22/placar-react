f697e7c6649e02986e75db817652e8d039bc8233
Renan Rodrigo <renanrb@ime.usp.br>
2018-08-01 09:54:43 -0300

    Adicionar state para mudar o placar

--- src/Pontuacao.js
+++ src/Pontuacao.js
@@ -5,9 +5,22 @@ class Pontuacao extends Component {
     constructor(props){
         super(props);
         this.time = "Time da casa";
+        this.state = {
+            gols: 0
+        };
+    }
+
+    incrementar = () => {
+        this.setState(
+            {
+                gols: this.state.gols + 1
+            }
+        );
     }
 
     render() {
+        console.log(this.state);
+
         if(this.props.visitante === "true"){
             this.time = "Time visitante";
         }
@@ -15,7 +28,10 @@ class Pontuacao extends Component {
         return (
             <div className="pontuacao">
                 <h1>{this.props.nomeDoTime || this.time}</h1>
-                <h2>0</h2>
+                <h2>{this.state.gols}</h2>
+                <div className="gol" onClick={this.incrementar}>
+                    GOL!
+                </div>
             </div>
         );
     }
